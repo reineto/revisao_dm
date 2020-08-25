c674e97cabe8157b8c3f8a30f147c45e78f74f05
reineto <50115119+reineto@users.noreply.github.com>
2020-08-24 21:41:50 -0300

    1

--- src/revisão/Main.java
+++ src/revisão/Main.java
@@ -1,11 +1,12 @@
-package revisão;
+//Reinaldo De Araujo Sandim Neto RA:00096475
+package revisï¿½o;
 
 import java.util.ArrayList;
 
 public class Main {
 	
 	public static void main (String[] args) {
-		
+	//EX A	
 		Cachorro dog1 = new Cachorro(null, null);
 		dog1.setNome("Danilo");
 		dog1.setRaca("pinscher");
@@ -13,15 +14,16 @@ public class Main {
 		Gato ga1 = new Gato(null, null);
 		ga1.setNome("Gustavo");
 		ga1.setRaca("Persa");
-		
+
+	//EX B 	
 	System.out.println(dog1.nome + " esta " + dog1.caminha());
 	System.out.println(ga1.nome + " esta " + ga1.caminha());
 
 	System.out.println("");
-
+	//EX C e D	
 	System.out.println(dog1.nome + " diz: " + dog1.late());
 	System.out.println(ga1.nome + " diz: " + ga1.mia());
-	
+	//EX E
 	ArrayList<Animal> array1 = new ArrayList<Animal>();
 	array1.add(dog1);
 	array1.add(ga1);
